e5a83e92ee642d6e5871a1bac86b1d849ae31eb7
joey-quadros-flex <joey.quadros@flexconsulta.com.br>
2023-04-04 20:04:02 -0300

    feat: add api connection

---
+++ src/API/api.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+const {API} = process.env
+
+const apiURLs = {
+  development: "http://127.0.0.1:3333/_api",
+  production: API,
+};
+
+// const apiFlex = axios.create({baseURL: apiURLs.development});
+const apiFlex = axios.create({ baseURL: apiURLs.production });
+
+module.exports = { apiFlex };
