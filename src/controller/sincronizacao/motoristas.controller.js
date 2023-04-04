80496595a52d0c6ab42e83733a1fe6e2ae1168bc
joey-quadros-flex <joey.quadros@flexconsulta.com.br>
2023-04-04 20:06:25 -0300

    feat: changed parameters and add api post to logs

--- src/controller/sincronizacao/motoristas.controller.js
+++ src/controller/sincronizacao/motoristas.controller.js
@@ -2,9 +2,9 @@ const moment = require('moment');
 const { MotoristaGetAll, MotoristaSetData } = require('../../config/sql/motoristas.js');
 const GerarArquivo = require('../../utils/gerador.arquivo.js');
 const sequelizePostgres = require('../../services/sequelize.service');
-const { pegarUmaEmpresa } = require('../../models/empresa');
-const { criarLogSincronizacao } = require('../../models/log.sincronizacao');
-const { encryptedData } = require('../../utils/encriptacao')
+const { encryptedData } = require('../../utils/encriptacao');
+const { apiFlex } = require('../../API/api.js');
+const { CNPJ } = process.env;
 
 class Motoristas extends GerarArquivo {
 
@@ -24,45 +24,48 @@ class Motoristas extends GerarArquivo {
 
             const data = { data: value }
             data.query_sql = value ? MotoristaSetData(moment(value, ['DD/MM/YYY HH:mm', "YYYY/MM/DD HH:mm"]).format("YYYY/MM/DD HH:mm")) : MotoristaGetAll()
-            this.empresa = await pegarUmaEmpresa()
-
             await this.sincronizar(data)
       }
 
       async sincronizar(data) {
             return new Promise(async (resolve) => {
-                  if (!this.empresa) return false;
-
-                  const dados = this.empresa
 
                   const dataConexao = {
-                        nome_banco: dados.nomebancodados,
-                        usuario_banco: dados.usuarioservidor,
-                        senha_banco: dados.senhaservidor,
-                        host_banco: dados.urlservidor,
-                        dialect: "postgres",
-                        porta_banco: dados.portaservidor
-                  }
+                    nome_banco: data.data.banco_server,
+                    usuario_banco: data.data.usuario_server,
+                    senha_banco: data.data.senha_server,
+                    host_banco: data.data.url_server,
+                    dialect: data.data.tipo_banco,
+                    porta_banco: Number(data.data.porta_server),
+                  };
 
                   const resultadoSequelize = await new sequelizePostgres(dataConexao)
-                  const arrayDados = await resultadoSequelize.obterDados(data.query_sql)
+                  const arrayDados = await resultadoSequelize.obterDados(MotoristaGetAll(0))
 
-                  let dadosLogs = null;
                   if (arrayDados?.length > 0) {
                         const dataEncriptado = await encryptedData(arrayDados)
                         await this.fnGeradorArquivos(dataEncriptado, 'SYNCz_MOTORISTAS', this.pastaSync)
 
-                        dadosLogs = {
-                              data: moment().format("YYYY/MM/DD HH:mm:ss"),
-                              qtd: arrayDados?.length || 0,
-                              tipo: 'SYNCz_MOTORISTAS',
-                              count: moment().format("YYYY/MM/DD HH:mm:ss").valueOf()
-                        }
+                        const dadosLogs = {
+                          idcliente: data.data.idcliente,
+                          cnpj_cliente: data.data.cnpj_empresa,
+                          entidade: "MOTORISTAS",
+                          error: false,
+                          nome_arquivo: "SYNCz_MOTORISTAS",
+                          data: moment().format("YYYY/MM/DD HH:mm:ss").valueOf(),
+                          id: data.data.id,
+                        };
 
-                        await criarLogSincronizacao(dadosLogs)
+                        await apiFlex.post(`/bootclient/log/?cnpj=${CNPJ}`, dadosLogs)
+                              .then((response) => {
+                                    console.log({ Log: response.data, msg: "Log Criado" });  
+                              })
+                              .catch((error) => {
+                                    console.log({ error, Where: "Sync Mot post Log" });  
+                              });
                   }
 
-                  console.log(`[MOTORISTAS X] || ${dadosLogs?.qtd || 0}`);
+                  console.log(`[MOTORISTAS X] || ${arrayDados?.length || 0}`);
                   resolve(true)
             })
 
