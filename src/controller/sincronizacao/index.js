454214deb7da5d6004f127d1d436a082019da2f4
joey-quadros-flex <joey.quadros@flexconsulta.com.br>
2023-04-04 20:05:46 -0300

    feat: implements api request to info emp

--- src/controller/sincronizacao/index.js
+++ src/controller/sincronizacao/index.js
@@ -5,10 +5,29 @@ const Motoristas = require('./motoristas.controller');
 const Proprietarios = require('./proprietarios.controller');
 const Veiculos = require('./veiculos.controller');
 const Viagens = require('./viagens.controller');
+const { apiFlex } = require("../../API/api.js");
+const { decryptApiResponse } = require("../../utils/decryptApiResponse");
+const { CNPJ } = process.env;
 
 module.exports = async (req, res) => {
+      const empresa = await apiFlex.get("/connection/?cnpj="+CNPJ)
+            .then((response) => {
+            const data = decryptApiResponse(response.data.data);
+            return data
+            })
+            .catch((error) => {
+            console.log({ error, Where: "Sync Index Get Empresa" });
+            });
 
-      let empresa = await pegarUmaEmpresa()
+      // const logs = await apiFlex.get("/bootclient/log/last/?cnpj=42.520.419/0001-14")
+      //       .then((response) => {
+      //       const data = decryptApiResponse(response.data.data);
+      //       return data
+      //       })
+      //       .catch((error) => {
+      //       console.log({ error, Where: "ClientDataReq controller" });
+      //       });
+
       const _DATAINICIAL = process.env.dtInitTravelDefault
 
       if (res && !empresa) res.status(404).send('Nenhuma empresa foi cadastrada!')
@@ -23,16 +42,15 @@ module.exports = async (req, res) => {
             ? moment(dt, ['DD/MM/YYY HH:mm', "YYYY/MM/DD HH:mm"]).format("YYYY/MM/DD HH:mm") : null;
 
 
-      const log_motorista = await log_sincronizacao({ tipo: "SYNCz_MOTORISTAS" })
-      const log_proprietario = await log_sincronizacao({ tipo: "SYNCz_PROPRIETARIOS" })
-      const log_veiculos = await log_sincronizacao({ tipo: "SYNCz_VEICULOS" })
-      const log_viagens = await log_sincronizacao({ tipo: "SYNCz_VIAGENS" })
+      // const log_proprietario = await log_sincronizacao({ tipo: "SYNCz_PROPRIETARIOS" })
+      // const log_veiculos = await log_sincronizacao({ tipo: "SYNCz_VEICULOS" })
+      // const log_viagens = await log_sincronizacao({ tipo: "SYNCz_VIAGENS" })
 
       Promise.all([
-            new Motoristas(log_motorista?.data || null),
-            new Proprietarios(log_proprietario?.data || null),
-            new Veiculos(log_veiculos?.data || null),
-            new Viagens(log_viagens?.data || _DATAINICIAL),
+            new Motoristas(empresa || null),
+            // new Proprietarios(log_proprietario?.data || null),
+            // new Veiculos(log_veiculos?.data || null),
+            // new Viagens(log_viagens?.data || _DATAINICIAL),
       ]).then(data => {
             if (res) res.status(202).send()
             console.log('Sincronização concluída com sucesso!');
