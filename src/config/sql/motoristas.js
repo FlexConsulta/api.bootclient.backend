195017534e75d2667595c668704b018fd8662a67
joey-quadros-flex <joey.quadros@flexconsulta.com.br>
2023-04-05 09:30:13 -0300

    fix: sql mot

--- src/config/sql/motoristas.js
+++ src/config/sql/motoristas.js
@@ -1,6 +1,6 @@
-const LIMIT = process.env.SQL_LIMIT
+const LIMIT = process.env.SQL_LIMIT;
 
-const MotoristaGetAll = (offset) => `SELECT mot.codmotorista, 
+const MotoristaGetAll = ({ offset = 0 }) => `SELECT mot.codmotorista, 
 coalesce(mot.dataatual, mot.datainclusao) AS dataatual,
 mot.cpf,
 mot.liberado,
@@ -11,8 +11,7 @@ mot.bloqueadoadm
 FROM motorista mot 
 WHERE mot.codmotorista = (select mot2.codmotorista from motorista mot2 where mot.cpf = mot2.cpf order by coalesce(mot2.dataatual, mot2.datainclusao) desc limit 1 )
 ORDER BY coalesce(mot.dataatual, mot.datainclusao) ASC
-LIMIT ${LIMIT} OFFSET ${offset};`
-
+LIMIT ${LIMIT} OFFSET ${offset};`;
 
 const MotoristaSetData = (data) => `SELECT mot.codmotorista, 
 coalesce(mot.dataatual, mot.datainclusao) AS dataatual,
@@ -25,6 +24,6 @@ mot.bloqueadoadm
 FROM motorista mot 
 WHERE coalesce(mot.dataatual, mot.datainclusao) >= '${data}' 
 AND mot.codmotorista = (select mot2.codmotorista from motorista mot2 where mot.cpf = mot2.cpf order by coalesce(mot2.dataatual, mot2.datainclusao) desc limit 1 )
-ORDER BY coalesce(mot.dataatual, mot.datainclusao) ASC;`
+ORDER BY coalesce(mot.dataatual, mot.datainclusao) ASC;`;
 
-module.exports = { MotoristaGetAll, MotoristaSetData }
+module.exports = { MotoristaGetAll, MotoristaSetData };
