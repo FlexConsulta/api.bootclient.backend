38e42800f0f425d78657c3a22f08d3071498ef80
joey-quadros-flex <joey.quadros@flexconsulta.com.br>
2023-04-05 11:34:56 -0300

    feat: Motoristas class

--- src/controller/sincronizacao/motoristas.controller.js
+++ src/controller/sincronizacao/motoristas.controller.js
@@ -4,15 +4,15 @@ const GerarArquivo = require('../../utils/gerador.arquivo.js');
 const sequelizePostgres = require('../../services/sequelize.service');
 const { encryptedData } = require('../../utils/encriptacao');
 const { apiFlex } = require('../../API/api.js');
-const { CNPJ } = process.env;
+const { CNPJ, SQL_LIMIT } = process.env;
 
 class Motoristas extends GerarArquivo {
 
-      constructor(value) {
+      constructor(value, empresa) {
             super()
 
             this.pastaSync = process.env.FOLDER_SYNC_SUCCESS
-            this.empresa = null
+            this.empresa = empresa;
 
             return new Promise(async resolve => {
                   await this.fnStart(value)
@@ -24,49 +24,69 @@ class Motoristas extends GerarArquivo {
 
             const data = { data: value }
             data.query_sql = value ? MotoristaSetData(moment(value, ['DD/MM/YYY HH:mm', "YYYY/MM/DD HH:mm"]).format("YYYY/MM/DD HH:mm")) : MotoristaGetAll()
-            await this.sincronizar(data)
+
+            await this.sincronizar(data);
       }
 
       async sincronizar(data) {
             return new Promise(async (resolve) => {
 
+                  const dados = this.empresa;
+
                   const dataConexao = {
-                    nome_banco: data.data.banco_server,
-                    usuario_banco: data.data.usuario_server,
-                    senha_banco: data.data.senha_server,
-                    host_banco: data.data.url_server,
-                    dialect: data.data.tipo_banco,
-                    porta_banco: Number(data.data.porta_server),
+                    nome_banco: dados.banco_server,
+                    usuario_banco: dados.usuario_server,
+                    senha_banco: dados.senha_server,
+                    host_banco: dados.url_server,
+                    dialect: dados.tipo_banco,
+                    porta_banco: Number(dados.porta_server),
                   };
 
-                  const resultadoSequelize = await new sequelizePostgres(dataConexao)
-                  const arrayDados = await resultadoSequelize.obterDados(MotoristaGetAll(0))
+                  let arquivos = 0
+                  let offset = 0;
+                  while(true){
 
-                  if (arrayDados?.length > 0) {
-                        const dataEncriptado = await encryptedData(arrayDados)
-                        await this.fnGeradorArquivos(dataEncriptado, 'SYNCz_MOTORISTAS', this.pastaSync)
+                        // setar a query
+                        let query;
+                        if(!data.data){
+                          const replaceParameters = (string, offset) => {
+                            const arr = string.split("{");
+                            arr.splice(1, 1, `${SQL_LIMIT} OFFSET `);
+                            arr.splice(2, 1, `${offset}`);
+                            string = String(arr.join(""));
+                            return string;
+                          };
+                          query = replaceParameters(data.query_sql, offset);
+                        } else query = data.query_sql;
 
-                        const dadosLogs = {
-                          idcliente: data.data.idcliente,
-                          cnpj_cliente: data.data.cnpj_empresa,
-                          entidade: "MOTORISTAS",
-                          error: false,
-                          nome_arquivo: "SYNCz_MOTORISTAS",
-                          data: moment().format("YYYY/MM/DD HH:mm:ss").valueOf(),
-                          id: data.data.id,
-                        };
+                        // faz a query
+                        const resultadoSequelize = await new sequelizePostgres(dataConexao)
+                        const arrayDados = await resultadoSequelize.obterDados(query);
 
-                        await apiFlex.post(`/bootclient/log/?cnpj=${CNPJ}`, dadosLogs)
-                              .then((response) => {
-                                    console.log({ Log: response.data, msg: "Log Criado" });  
-                              })
-                              .catch((error) => {
-                                    console.log({ error, Where: "Sync Mot post Log" });  
-                              });
+                        // criar arquivo
+                        if (arrayDados?.length > 0) {
+                              const dataEncriptado = await encryptedData(arrayDados)
+                              await this.fnGeradorArquivos(dataEncriptado, 'SYNCz_MOTORISTAS', CNPJ , this.pastaSync)
+
+                              const dadosLogs = {
+                                    cnpj_cliente: CNPJ,
+                                    error: false,
+                                    nome_arquivo: `SYNCz_MOTORISTAS_${CNPJ}`,
+                                    entidade: "MOTORISTAS",
+                              };
+
+                              // gera o log
+                              await apiFlex.post(`/bootclient/log?cnpj=${CNPJ}`, dadosLogs)
+                        }
+
+                        console.log(`[MOTORISTAS X] || ${arrayDados?.length || 0}`);
+                        arquivos++;
+                        if(arrayDados.length < SQL_LIMIT ) break 
+                        offset += SQL_LIMIT;
                   }
+                  console.log({arquivos})
+                  resolve(true);
 
-                  console.log(`[MOTORISTAS X] || ${arrayDados?.length || 0}`);
-                  resolve(true)
             })
 
       }
