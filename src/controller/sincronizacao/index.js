bfc247ba4b9b0ebc62de47e72ee21842b927d30c
joey-quadros-flex <joey.quadros@flexconsulta.com.br>
2023-04-05 18:30:48 -0300

    feat: new verification for empresas

--- src/controller/sincronizacao/index.js
+++ src/controller/sincronizacao/index.js
@@ -1,11 +1,10 @@
-const moment = require('moment');
 const Motoristas = require('./motoristas.controller');
 const Proprietarios = require('./proprietarios.controller');
 const Veiculos = require('./veiculos.controller');
 const Viagens = require('./viagens.controller');
 const { apiFlex } = require("../../API/api.js");
 const { decryptApiResponse } = require('../../utils/decryptApiResponse');
-const { CNPJ, DATAINICIAL } = process.env;
+const { CNPJ } = process.env;
 
 module.exports = async (req, res) => {
 
@@ -18,23 +17,20 @@ module.exports = async (req, res) => {
             return;
       }
 
-      const dt = empresa?.datasincronizacao || null
-      datasincronizacao = dt && moment(dt, ['DD/MM/YYY HH:mm', "YYYY/MM/DD HH:mm"]).isValid()
-            ? moment(dt, ['DD/MM/YYY HH:mm', "YYYY/MM/DD HH:mm"]).format("YYYY/MM/DD HH:mm") : null;
-
-      const logs = await apiFlex.get(`/bootclient/log/last?cnpj=${CNPJ}`);
+      if (!data_empresa?.ativo) {
+            console.log(`A empresa [${data_empresa.nomefantasia_empresa}] foi desativada!`)
+            return;
+      }
 
       Promise.all([
-            // new Motoristas(logs.data["motoristas"]?.data || null, data_empresa),
-            // new Proprietarios(logs["proprietarios"]?.data || null, data_empresa),
-            // new Veiculos(logs["veiculos"]?.data || null, data_empresa),
-            // new Viagens(logs["viagens"]?.data || DATAINICIAL, data_empresa),
+            new Motoristas(data_empresa),
+            // new Proprietarios(logs["proprietarios"]?.data || null),
+            // new Veiculos(logs["veiculos"]?.data || null),
+            // new Viagens(logs["viagens"]?.data || DATAINICIAL),
       ]).then(data => {
             if (res) res.status(202).send()
             console.log('Sincronização concluída com sucesso!');
       }).catch(error => {
             console.log(error);
       })
-
-
 }
