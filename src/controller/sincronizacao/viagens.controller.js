a216a5a2da9aa37ea8b3f388ed8ba2b06cb3a587
joey-quadros-flex <joey.quadros@flexconsulta.com.br>
2023-04-06 16:24:50 -0300

    feat: req to lastLog and impls sql from empresa dt

--- src/controller/sincronizacao/viagens.controller.js
+++ src/controller/sincronizacao/viagens.controller.js
@@ -3,47 +3,8 @@ const GerarArquivo = require('../../utils/gerador.arquivo.js');
 const sequelizePostgres = require('../../services/sequelize.service');
 const { encryptedData } = require('../../utils/encriptacao')
 const { fnGerarLogs } = require("../../utils/gerarLogs.js");
+const { apiFlex } = require('../../API/api.js');
 const { SQL_LIMIT, FOLDER_SYNC_SUCCESS, DATAINICIAL } = process.env;
-
-const SQL = `SELECT c.data,
-coalesce(c.dataatual, c.datadigitacao) AS dataatual,
-c.numero,
-c.cancelado,
-coalesce(mot.dataatual, mot.datainclusao) AS dataatualmot,
-mot.cpf AS cpfmot,
-mot.nome AS nomemot,
-mot.liberado AS liberadomot,
-mot.bloqueadoadm AS bloqueadoadmmot,
-mot.celular AS celularmot,
-mot.fone AS fonemot,
-coalesce(prop.dataatual, prop.datainclusao) AS dataatualprop,
-prop.cgccpf AS cgccpf,
-prop.nome AS nomeprop,
-prop.liberado AS liberadoprop,
-prop.bloqueadoadm AS bloqueadoadmprop,
-coalesce(veic.dataatual, veic.datainclusao) AS dataatualveic,
-veic.placa AS cavalo,
-veic.liberado AS liberadoveic,
-veic.bloqueadoadm AS bloqueadoadmveic,
-veic.placacarreta AS carreta1,
-veic.placacarreta2 AS carreta2,
-veic.placacarreta3 AS carreta3,
-veic.placacarreta4 AS carreta4,
-merc.descricao AS mercadoria,
-cid.nome AS nomecidadeorigem,
-cid.uf AS ufcidadeorigem,
-cidd.nome AS nomecidadedestino,
-cidd.uf AS ufcidadedestino
-FROM conhecimento c
-INNER JOIN motorista mot ON (c.codmotorista = mot.codmotorista)
-INNER JOIN veiculo veic ON (c.codveiculo = veic.codveiculo)
-INNER JOIN proprietario prop ON (c.codproprietario = prop.codproprietario)
-INNER JOIN mercadoria merc ON (c.codmercadoria = merc.codmercadoria)
-LEFT OUTER JOIN cidade cid ON (c.codcidadeorigem = cid.codcidade)
-LEFT OUTER JOIN cidade cidd ON (c.codcidadedestino = cidd.codcidade)
-WHERE c.data >= '$' AND c.data <= now() AT TIME ZONE 'AST'
-AND c.tipocte IN ('0','1','2','B','D')
-ORDER BY c.data ASC`;
 
 class Viagens extends GerarArquivo {
   constructor(empresa) {
@@ -66,13 +27,32 @@ class Viagens extends GerarArquivo {
         porta_banco: Number(this.empresa.porta_server),
       };
 
-      const data = moment(DATAINICIAL, [ "DD/MM/YYY HH:mm", "YYYY/MM/DD HH:mm",]).format("YYYY/MM/DD HH:mm");
-      const _SQL = SQL.replace("$", data);
+      // req last log
+      const logs = await apiFlex.get(`bootclient/log/last?cnpj=${this.empresa.cnpj_empresa}`);
+      const lastSyncDate = logs.data["viagens"].data;
+      console.log(lastSyncDate)
+
+      // sql json -> obj
+      let SQL_object = this.empresa.sql_viagens;
+      SQL_object = JSON.parse(SQL_object)
+
+      // definir sql
+      let SQL;
+      if(lastSyncDate){
+         SQL = SQL_object.setByDataFiltred;
+         const data_query = moment(lastSyncDate, [ "DD/MM/YYY HH:mm", "YYYY/MM/DD HH:mm",]).format("YYYY/MM/DD HH:mm");
+         SQL = SQL.replace("[$]", data_query);
+      }else{
+         SQL = SQL_object.setByData;
+         const data_query = moment(DATAINICIAL, [ "DD/MM/YYY HH:mm", "YYYY/MM/DD HH:mm",]).format("YYYY/MM/DD HH:mm");
+         SQL = SQL.replace("[$]", data_query);
+      }
+
       let offset = 0;
       
       for (let i = 0; ; i++) {
         // SETAR SQL
-        const _sql = `${_SQL} LIMIT ${SQL_LIMIT} OFFSET ${offset}`;
+        const _sql = `${SQL} LIMIT ${SQL_LIMIT} OFFSET ${offset}`;
 
         // get dados
         const resultadoSequelize = await new sequelizePostgres(dataConexao);
@@ -81,16 +61,16 @@ class Viagens extends GerarArquivo {
         // encripta
         const dataEncriptado = await encryptedData(arrayDados);
 
-        // gerar arquivo
-        await this.fnGeradorArquivos(
-          dataEncriptado,
-          "SYNCz_VIAGENS",
-          this.empresa.cnpj_empresa,
-          FOLDER_SYNC_SUCCESS
-        );
+        if (arrayDados?.length > 0){
+          // gerar arquivo
+          await this.fnGeradorArquivos(
+            dataEncriptado,
+            "SYNCz_VIAGENS",
+            this.empresa.cnpj_empresa,
+            FOLDER_SYNC_SUCCESS
+          );
 
-        // gerar log
-        if (arrayDados?.length > 0)
+          // gerar log
           await fnGerarLogs(
             this.empresa.cnpj_empresa,
             "SYNCz_VIAGENS",
@@ -98,6 +78,7 @@ class Viagens extends GerarArquivo {
             "viagens",
             arrayDados.length
           );
+        }
 
         console.log(`[VIAGENS X] || ${arrayDados?.length || 0}`);
         if (arrayDados.length < SQL_LIMIT) break;
