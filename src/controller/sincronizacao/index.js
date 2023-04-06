c3d9b0834aaf41ab04760d6be98b0689cfbadc4a
joey-quadros-flex <joey.quadros@flexconsulta.com.br>
2023-04-06 16:25:23 -0300

    feat: comment classes

--- src/controller/sincronizacao/index.js
+++ src/controller/sincronizacao/index.js
@@ -7,30 +7,33 @@ const { decryptApiResponse } = require('../../utils/decryptApiResponse');
 const { CNPJ } = process.env;
 
 module.exports = async (req, res) => {
+  const empresa = await apiFlex.get(`/connection/?cnpj=${CNPJ}`);
+  const data_empresa = decryptApiResponse(empresa.data.data);
 
-      const empresa = await apiFlex.get(`/connection/?cnpj=${CNPJ}`)
-      const data_empresa = decryptApiResponse(empresa.data.data);
+  if (res && !empresa) res.status(404).send("Nenhuma empresa foi cadastrada!");
+  if (!empresa) {
+    console.log("Nenhuma empresa foi cadastrada!");
+    return;
+  }
 
-      if (res && !empresa) res.status(404).send('Nenhuma empresa foi cadastrada!')
-      if (!empresa) {
-            console.log('Nenhuma empresa foi cadastrada!')
-            return;
-      }
+  if (!data_empresa?.ativo) {
+    console.log(
+      `A empresa [${data_empresa.nomefantasia_empresa}] foi desativada!`
+    );
+    return;
+  }
 
-      if (!data_empresa?.ativo) {
-            console.log(`A empresa [${data_empresa.nomefantasia_empresa}] foi desativada!`)
-            return;
-      }
-
-      Promise.all([
-            // new Motoristas(data_empresa),
-            // new Proprietarios(data_empresa),
-            // new Veiculos(data_empresa),
-            // new Viagens(data_empresa),
-      ]).then(data => {
-            if (res) res.status(202).send()
-            console.log('Sincronização concluída com sucesso!');
-      }).catch(error => {
-            console.log(error);
-      })
+  Promise.all([
+//     new Motoristas(data_empresa),
+//     new Proprietarios(data_empresa),
+//     new Veiculos(data_empresa),
+//     new Viagens(data_empresa),
+  ])
+    .then((data) => {
+      if (res) res.status(202).send();
+      console.log("Sincronização concluída com sucesso!");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
