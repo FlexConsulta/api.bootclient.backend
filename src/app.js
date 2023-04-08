30f69c855015c404a7f732bc9d05cc84d936a83e
joey-quadros-flex <joey.quadros@flexconsulta.com.br>
2023-04-08 10:53:49 -0300

    feat: add VerificacaoAutomatica

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const RouteLogs = require('./routes/logs.routes')
 
 const MonitoramentoArquivos = require('./controller/monitoramento')
 const SincronizacaoAutomatica = require('./controller/sincronizacao.automatica')
+const VerificacaoAutomatica = require("./controller/verificacao.automatica");
 const SincronizacaoAutomaticaBackup = require('./controller/sincronizacao.automatica/sincronizacao.backup')
 const LimpezaLogsSistema = require('./controller/log.sincronizacoes/limpeza.automatica.logs')
 const MonitoramentoArquivosNaoEnviados = require("./controller/coletar.informacoes/arquivos.pendentes")
@@ -41,22 +42,13 @@ app.use(RouteLogs)
  *      TODO: Total viagens cancelados  (Diariamente -1)
  */
 
-new MonitoramentoArquivos(new Date())
-new SincronizacaoAutomatica(new Date())
-new MonitoramentoArquivosNaoEnviados()
-new FuncionamentoBootclient(new Date())
+// new MonitoramentoArquivos(new Date())
+// new SincronizacaoAutomatica(new Date())
+new VerificacaoAutomatica();
+// new MonitoramentoArquivosNaoEnviados()
+// new FuncionamentoBootclient(new Date())
 
 // new SincronizacaoAutomaticaBackup(new Date())
 // new LimpezaLogsSistema(new Date())
 
 module.exports = app;
-
-// {
-//     getAll: SELECT
-//     getByDate: SELECT
-//     count: SELECT count(mot.codmotorista) FROM motorista mot LIMIT 1
-// }
-// SELECT count(mot.codmotorista) FROM motorista mot LIMIT 1
-// SELECT count(c.numero) FROM conhecimento c LIMIT 1
-// SELECT count(veic.placa) FROM veiculo veic  LIMIT 1
-// SELECT count(prop.cgccpf) FROM proprietario prop  LIMIT 1
