9ffd346eef224ed0d838da089daa0f3ce1abaac2
joey-quadros-flex <joey.quadros@flexconsulta.com.br>
2023-04-08 11:36:19 -0300

    feat: implements getInfoCompany function

--- src/controller/sincronizacao/index.js
+++ src/controller/sincronizacao/index.js
@@ -2,33 +2,13 @@ const Motoristas = require('./motoristas.controller');
 const Proprietarios = require('./proprietarios.controller');
 const Veiculos = require('./veiculos.controller');
 const Viagens = require('./viagens.controller');
-const { apiFlex } = require("../../API/api.js");
-const { decryptApiResponse } = require('../../utils/decryptApiResponse');
-const { CNPJ } = process.env;
+const getInfoCompany = require('../../utils/get.info.company');
+const { apiFlex } = require('../../API/api');
 
 module.exports = async (req, res) => {
-    const empresa = await apiFlex.get(`/connection/?cnpj=${CNPJ}`);
-    const data_empresa = decryptApiResponse(empresa.data.data);
 
-    if (res && !empresa) res.status(404).send("Nenhuma empresa foi cadastrada!");
-    if (!empresa) {
-        console.log("Nenhuma empresa foi cadastrada!");
-        return;
-    }
-
-    if (!data_empresa?.ativo) {
-        console.log(`A empresa [${data_empresa.nomefantasia_empresa}] foi desativada!`);
-        return;
-    }
-
-    const dbObjectConnection = {
-        nome_banco: data_empresa.banco_server,
-        usuario_banco: data_empresa.usuario_server,
-        senha_banco: data_empresa.senha_server,
-        host_banco: data_empresa.url_server,
-        dialect: data_empresa.tipo_banco,
-        porta_banco: Number(data_empresa.porta_server),
-    }
+    const { dbObjectConnection, data_empresa } = await getInfoCompany()
+    if (!data_empresa) res.status(404).send("Nenhuma empresa foi cadastrada!");
 
     const logs = await apiFlex.get(`bootclient/log/last?cnpj=${data_empresa.cnpj_empresa}`);
     const { motoristas, proprietarios, veiculos, viagens } = logs.data
