7205105d1ce5e4b443b83d5b3d872ada649b01f0
Ricardo Gomes <ricardo@flexconsulta.com.br>
2023-04-10 14:08:49 -0300

    up

--- src/controller/auto.update/index.js
+++ src/controller/auto.update/index.js
@@ -8,16 +8,10 @@ const options = {
     trimmed: false,
 };
 
-const repo = simpleGit(process.env.GIT_PATH, options).clean(
-    CleanOptions.FORCE
-);
-// const repo = simpleGit("https://github.com/anaelj/autoupdate", options).clean(
-//     CleanOptions.FORCE
-// );
+const repo = simpleGit(process.env.GIT_PATH, options).clean(CleanOptions.FORCE);
+const intervaloAtualizacao = "*/2 * * * *"; 
 
-const intervaloAtualizacao = "*/2 * * * *"; // atualiza a cada 5 minutos
-
- const AutoComplete = cron.schedule(intervaloAtualizacao, () => {
+const AutoComplete = cron.schedule(intervaloAtualizacao, () => {
     console.log("Verificando atualizações...");
     repo.pull((err, atualizacao) => {
         if (atualizacao && atualizacao.summary.changes) {
