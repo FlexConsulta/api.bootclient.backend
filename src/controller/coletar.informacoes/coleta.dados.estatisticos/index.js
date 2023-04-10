5ed4a9a5a35ee65c918544117b1171026194b9bb
Ricardo Gomes <ricardo@flexconsulta.com.br>
2023-04-10 09:19:09 -0300

    Up

--- src/controller/coletar.informacoes/coleta.dados.estatisticos/index.js
+++ src/controller/coletar.informacoes/coleta.dados.estatisticos/index.js
@@ -17,14 +17,50 @@ class ColetaDadosEstatisticos {
         try {
             const { dbObjectConnection, data_empresa } = await getInfoCompany()
 
-            const logs = await apiFlex.get(`bootclient/log/last?cnpj=${data_empresa.cnpj_empresa}`);
-            const { motoristas, proprietarios, veiculos, viagens } = logs.data
+            // {
+            //     count_motoristas:"SELECT * ..."
+            //     count_PROPRIETARIOS:"SELECT * ..."
+            //     ...
+            // }
+
+
+            return
+
+
+            
+
+            // sql query p/ cada sql
+            for (let key in this.dbSQL) {
+
+                // fznd sql com base no last log ou data inicial
+                const _SQL = this.dbSQL[key].replace(
+                    "[$]",
+                    moment(this.lastSyncDate, [
+                        "DD/MM/YYY HH:mm",
+                        "YYYY/MM/DD HH:mm",
+                    ]).format("YYYY/MM/DD HH:mm") || DATAINICIAL
+                );
+                const resultadoSequelize = await new sequelizePostgres(this.dbObjectConnection);
+                const query = await resultadoSequelize.obterDados(_SQL);
+                sql_motoristas.push(query.length);
+            }
+
+            const rsltLogsRegister = await fnGerarLogs({
+                cnpj_cliente: this.cnpj_empresa,
+                nome_arquivo: null,
+                error: false,
+                entidade: "DADOS_ESTATISTICOS",
+                quantidade: `[${String(sql_motoristas)}]`,
+                categoria: "DADOS_ESTATISTICOS_MOTORISTAS",
+                mensagem: "coleta de dados estatísticos dos motoristas concluída com sucesso!",
+            });
+            console.log({ rsltLogsRegister });
 
             Promise.all([
-              new Motoristas({ dbObjectConnection, cnpj_empresa: data_empresa.cnpj_empresa, dbSQL: data_empresa.sql_motoristas, lastSyncDate: motoristas?.data }),
-              new Proprietarios({ dbObjectConnection, cnpj_empresa: data_empresa.cnpj_empresa, dbSQL: data_empresa.sql_proprietarios, lastSyncDate: proprietarios?.data }),
-              new Veiculos({ dbObjectConnection, cnpj_empresa: data_empresa.cnpj_empresa, dbSQL: data_empresa.sql_veiculos, lastSyncDate: veiculos?.data }),
-              new Viagens({ dbObjectConnection, cnpj_empresa: data_empresa.cnpj_empresa, dbSQL: data_empresa.sql_viagens, lastSyncDate: viagens?.data }),
+                new Motoristas({ dbObjectConnection, cnpj_empresa: data_empresa.cnpj_empresa, dbSQL: data_empresa.sql_motoristas, lastSyncDate: motoristas?.data }),
+                new Proprietarios({ dbObjectConnection, cnpj_empresa: data_empresa.cnpj_empresa, dbSQL: data_empresa.sql_proprietarios, lastSyncDate: proprietarios?.data }),
+                new Veiculos({ dbObjectConnection, cnpj_empresa: data_empresa.cnpj_empresa, dbSQL: data_empresa.sql_veiculos, lastSyncDate: veiculos?.data }),
+                new Viagens({ dbObjectConnection, cnpj_empresa: data_empresa.cnpj_empresa, dbSQL: data_empresa.sql_viagens, lastSyncDate: viagens?.data }),
             ]).then((data) => {
                 console.log("Coleta de dados estatísticos concluída com sucesso!");
             }).catch((error) => {
