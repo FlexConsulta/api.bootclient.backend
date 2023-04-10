12f325e4c207b3a00db5310fa5429cfa9430f90a
joey-quadros-flex <joey.quadros@flexconsulta.com.br>
2023-04-10 17:19:17 -0300

    init log

--- src/controller/coletar.informacoes/conexaoDbCliente.js
+++ src/controller/coletar.informacoes/conexaoDbCliente.js
@@ -9,6 +9,33 @@ class ConexaoDbCliente {
 
     async start() {
         try {
+            const { dbObjectConnection, data_empresa } = await getInfoCompany();
+            let sqls = {
+                count_motoristas: JSON.parse(data_empresa.sql_motoristas).getOne,
+                // count_proprietarios: JSON.parse(data_empresa.sql_proprietarios).getOne,
+                // count_veiculos: JSON.parse(data_empresa.sql_veiculos).getOne,
+                // count_viagens: JSON.parse(data_empresa.sql_viagens).getOne,
+            };
+            console.log({sqls})
+            return
+            const arraySqls = [];
+            
+            for (const key in sqls) {
+                let sql = sqls[key]
+                const resultadoSequelize = await new sequelizePostgres(dbObjectConnection);
+                const query_result = await resultadoSequelize.obterDados(sql)
+                arraySqls.push({ [key]: query_result[0].count });
+            }
+
+            const rsltLogsRegister = await fnGerarLogs({
+                cnpj_cliente: data_empresa.cnpj_empresa,
+                nome_arquivo: null,
+                error: false,
+                entidade: "DADOS_ESTATISTICOS",
+                quantidade: JSON.stringify(arraySqls),
+                categoria: "DADOS_ESTATISTICOS_COUNT",
+                mensagem: "coleta de dados estatísticos concluída com sucesso!",
+            });
 
         } catch (error) {
             console.log({ error });
