f654a3bd3f09568ab093cdb7790489757cebb1e0
Ricardo Gomes <ricardo@flexconsulta.com.br>
2023-04-10 14:16:59 -0300

    Up

--- src/controller/auto.update/index.js
+++ src/controller/auto.update/index.js
@@ -9,11 +9,15 @@ const options = {
 };
 
 const repo = simpleGit(process.env.GIT_PATH, options).clean(CleanOptions.FORCE);
-const intervaloAtualizacao = "*/2 * * * *"; 
+const intervaloAtualizacao = "*/10 * * * * *";
 
 const AutoComplete = cron.schedule(intervaloAtualizacao, () => {
     console.log("Verificando atualizações...");
     repo.pull((err, atualizacao) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
         if (atualizacao && atualizacao.summary.changes) {
             console.log(`Atualizado!`);
         } else {
