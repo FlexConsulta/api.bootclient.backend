25b4a0c3dda8b2ddb70c5de1ddc89f471e53323b
joey-quadros-flex <joey.quadros@flexconsulta.com.br>
2023-04-10 15:05:27 -0300

    feat: ConexaoDbCliente job

---
+++ src/controller/coletar.informacoes/conexaoDbClienteAutomatico.js
@@ -0,0 +1,28 @@
+
+const moment = require("moment");
+const schedule = require("node-schedule");
+const ConexaoDbCliente = require("./conexaoDbCliente");
+
+moment.locale("pt-br");
+const { JOB_DB_CONNECTION_AUTO } = process.env;
+
+class ConexaoDbClienteAutomatico {
+  constructor() {
+    (async () => {
+      try {
+        schedule.gracefulShutdown();
+        const fn = () => {
+          console.log(`[i] Monitoramento de arquivos não enviados: ${moment().format("LLLL")}`);
+          new ConexaoDbCliente();
+        };
+
+        fn();
+        schedule.scheduleJob(JOB_DB_CONNECTION_AUTO || "*/10 * * * * *", fn);
+      } catch (error) {
+        console.log({ error });
+      }
+    })();
+  }
+}
+
+module.exports = ConexaoDbClienteAutomatico;
