c2923ff8d12b9edcfd35189c7cc1c1db878d6e3d
joey-quadros-flex <joey.quadros@flexconsulta.com.br>
2023-04-11 11:10:55 -0300

    feat: await implements

--- src/controller/verificar.entidades/index.js
+++ src/controller/verificar.entidades/index.js
@@ -12,11 +12,11 @@ module.exports = async (req, res) => {
     if (!data_empresa) res.status(404).send("Nenhuma empresa foi cadastrada!");
 
     Promise.all([
-        new Motoristas({ dbObjectConnection, cnpj_empresa: data_empresa.cnpj_empresa, dbSQL: data_empresa.sql_motoristas}),
-        new Proprietarios({ dbObjectConnection, cnpj_empresa: data_empresa.cnpj_empresa, dbSQL: data_empresa.sql_proprietarios}),
-        new Veiculos({ dbObjectConnection, cnpj_empresa: data_empresa.cnpj_empresa, dbSQL: data_empresa.sql_veiculos}),
-        new Viagens({ dbObjectConnection, cnpj_empresa: data_empresa.cnpj_empresa, dbSQL: data_empresa.sql_viagens}),
-    ]).then(async () => {
+        await new Motoristas({ dbObjectConnection, cnpj_empresa: data_empresa.cnpj_empresa, dbSQL: data_empresa.sql_motoristas}),
+        await new Proprietarios({ dbObjectConnection, cnpj_empresa: data_empresa.cnpj_empresa, dbSQL: data_empresa.sql_proprietarios}),
+        await new Veiculos({ dbObjectConnection, cnpj_empresa: data_empresa.cnpj_empresa, dbSQL: data_empresa.sql_veiculos}),
+        await new Viagens({ dbObjectConnection, cnpj_empresa: data_empresa.cnpj_empresa, dbSQL: data_empresa.sql_viagens}),
+    ]).then(() => {
         throw new Error("Error manual");
 
         if (res) res.status(202).send();
