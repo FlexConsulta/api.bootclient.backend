307422c139230657a02b5abb3b6e14d278b6d27f
joey-quadros-flex <joey.quadros@flexconsulta.com.br>
2023-04-26 17:28:30 -0300

    feat: implements formatarData function

--- src/utils/gerarLogs.js
+++ src/utils/gerarLogs.js
@@ -1,11 +1,13 @@
 const { apiFlex } = require("../API/api");
+const { formatarData } = require("./tratamento.dados");
 
 const fnGerarLogs = async (dadosLogs) => {
   try {
     const d = await apiFlex.post(`/bootclient/log`, dadosLogs);
+    d.data.data = await formatarData(d.data.data);
     return d.data
   } catch (error) {
-    console.log({ error: error?.toJSON() || error, where: "fnGerarLogs" });
+    console.log({ erro: error && error.message ? JSON.stringify({ error: error.message }) : null, where: "fnGerarLogs" });
   }
 };
 
