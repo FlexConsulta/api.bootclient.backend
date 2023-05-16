4e5264dd322670e8c4986690990d226e8273c916
joey-quadros-flex <joey.quadros@flexconsulta.com.br>
2023-05-16 17:34:30 -0300

    feat: fix ; bug

--- src/controller/sincronizacao/viagens.controller.js
+++ src/controller/sincronizacao/viagens.controller.js
@@ -48,7 +48,10 @@ class Viagens extends GerarArquivo {
 
               for (let i = 0; ; i++) {
 
-                SQL = SQL.replace(";", " ");
+                if (SQL.includes(":")) {
+                  SQL = SQL.replace(";", " ");
+                }
+
                 const _sql = `${SQL} LIMIT ${SQL_LIMIT || 500} OFFSET ${offset};`;
                 
                 const resultadoSequelize = await new sequelizePostgres(this.dbObjectConnection);
