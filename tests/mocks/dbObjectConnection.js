6cccf6633cdef1eab6c441119ca6b7fe4f04f363
joey-quadros-flex <127412301+joey-quadros-flex@users.noreply.github.com>
2023-05-18 17:50:42 -0300

    Delete dbObjectConnection.js

--- tests/mocks/dbObjectConnection.js
+++
@@ -1,9 +0,0 @@
-const dbObjectConnection = {
-  nome_banco: "carvalho",
-  usuario_banco: "postgres",
-  senha_banco: "MasterNovel",
-  host_banco: "192.168.20.81",
-  dialect: "postgres",
-  porta_banco: 5432,
-};
-module.exports = { dbObjectConnection };
