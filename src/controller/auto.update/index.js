094bb2c70622497d6fd1c299c9a7f032d046a4fa
joey-quadros-flex <joey.quadros@flexconsulta.com.br>
2023-05-26 15:06:58 -0300

    feat: inital config to add brach option to update

--- src/controller/auto.update/index.js
+++ src/controller/auto.update/index.js
@@ -4,7 +4,8 @@ const { exec } = require('child_process');
 
 const moment = require("moment");
 moment.locale("pt-br");
-const { JOB_ATUALIZACAO_AUTOMATICA_CODIGO, PM2_PROCESS_RUNNING } = process.env
+const { JOB_ATUALIZACAO_AUTOMATICA_CODIGO, PM2_PROCESS_RUNNING, BRANCH_PROD } =
+  process.env;
 
 class SystemUpdateAuto {
     constructor() {
@@ -22,36 +23,46 @@ class SystemUpdateAuto {
                 process.on("SIGINT", function () {
                     schedule.gracefulShutdown().then(() => process.exit(0));
                 });
-                const fn = () => {
+                const fn = async () => {
                     
                     console.log('=========================================================');
                     console.log(`[i] Verificando atualizações do App: ${moment().tz('America/Sao_Paulo').format('LLL')}`);
                     console.log('=========================================================');
                     const gitRepository = simpleGit(process.env.GIT_PATH, options).clean(CleanOptions.FORCE);
 
-                    gitRepository.pull((err, atualizacao) => {
-                        if (err) {
-                            console.log(err);
-                            return;
-                        }
+                    const branchs = await gitRepository.branch()
+                    const branchAtual = branchs.current;
 
-                        if (atualizacao && atualizacao.summary.changes) {
+                    if(branchAtual != BRANCH_PROD){
+                        await gitRepository.checkoutBranch(BRANCH_PROD);
+                        fn();
+                        return;
+                    } else console.log({ branchAtual });
+                    console.log({ branchAtual });
 
-                            console.log(`[i] O sistema foi atualizado com sucesso: ${atualizacao.summary.changes}`);
-                            exec(`pm2 restart ${PM2_PROCESS_RUNNING}`, (err, stdout, stderr) => {
-                                if (err) {
-                                    console.error(`Erro ao reiniciar aplicação: ${err}`);
-                                    return;
-                                }
-                                console.log(`stdout: ${stdout}`);
-                                console.error(`stderr: ${JSON.stringify(stderr)}`);
-                            });
+                    // gitRepository.pull((err, atualizacao) => {
+                    //     if (err) {
+                    //         console.log(err);
+                    //         return;
+                    //     }
+
+                    //     if (atualizacao && atualizacao.summary.changes) {
+
+                    //         console.log(`[i] O sistema foi atualizado com sucesso: ${atualizacao.summary.changes}`);
+                    //         exec(`pm2 restart ${PM2_PROCESS_RUNNING}`, (err, stdout, stderr) => {
+                    //             if (err) {
+                    //                 console.error(`Erro ao reiniciar aplicação: ${err}`);
+                    //                 return;
+                    //             }
+                    //             console.log(`stdout: ${stdout}`);
+                    //             console.error(`stderr: ${JSON.stringify(stderr)}`);
+                    //         });
 
 
-                        } else {
-                            console.log("Nenhuma atualização disponível.");
-                        }
-                    });
+                    //     } else {
+                    //         console.log("Nenhuma atualização disponível.");
+                    //     }
+                    // });
                 };
 
                 fn();
